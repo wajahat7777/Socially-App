bf5e891428238a5d624da9551aec68ef41e51c5e
Wajahat <wajahatcru@gmail.com>
2025-09-07 16:21:08 +0500

    Boot via NavigationActivity and register activities

--- app/src/main/java/com/example/socially/MainActivity.kt
+++ app/src/main/java/com/example/socially/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.socially
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        
+        // Start with page 1 (splash screen)
+        val intent = Intent(this, NavigationActivity::class.java)
+        intent.putExtra("page", 1)
+        startActivity(intent)
+        finish()
     }
 }
